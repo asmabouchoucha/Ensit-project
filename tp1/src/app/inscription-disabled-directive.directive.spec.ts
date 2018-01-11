7a0cfa7ba5bd7ad58b7cf3c5b39b60f859708bc8
asmabouchoucha <moukamouka1234@gmail.com>
2018-01-11 23:29:28 +0100

    7

--- tp1/src/app/inscription-disabled-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InscriptionDisabledDirectiveDirective } from './inscription-disabled-directive.directive';
-
-describe('InscriptionDisabledDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InscriptionDisabledDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
